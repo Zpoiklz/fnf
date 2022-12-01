e8f738952b52b324dbb880b428e3be443db38964
Zpoiklz <119071582+Zpoiklz@users.noreply.github.com>
2022-11-30 22:34:23 -0300

    Add files via upload

---
+++ assets/preload/custom_events/gapplebounce.lua
@@ -0,0 +1,60 @@
+local gfSpeed = 1;
+
+function onBeatHit()
+
+	if (curBeat % gfSpeed == 0) then
+		if curBeat % (gfSpeed * 2) == 0 then
+			setProperty('iconP1.scale.x', 0.85 );
+			setProperty('iconP1.scale.y', 0.85 );
+			setProperty('iconP2.scale.x', 1.15);
+			setProperty('iconP2.scale.y', 1.15 );
+
+			setProperty('iconP1.angle', -15);
+			setProperty('iconP2.angle', 15);
+		else
+			setProperty('iconP1.scale.x', 1.15 );
+			setProperty('iconP1.scale.y', 1.15 );
+			setProperty('iconP2.scale.x', 0.85 );
+			setProperty('iconP2.scale.y', 0.85 );
+
+			setProperty('iconP2.angle', -15);
+			setProperty('iconP1.angle', 15);
+		end
+
+	end
+
+end
+
+function onUpdate()
+
+    if (getProperty('iconP1.angle') >= 0) then
+	    if ('iconP1.angle' ~= 0) then
+    	    setProperty('iconP1.angle', getProperty('iconP1.angle')-1);
+    	end
+    else
+        if ('iconP1.angle' ~= 0) then
+    	    setProperty('iconP1.angle', getProperty('iconP1.angle')+1);
+    	end
+    end
+
+    if (getProperty('iconP2.angle') >= 0) then
+	    if ('iconP2.angle' ~= 0) then
+    	    setProperty('iconP2.angle', getProperty('iconP2.angle')-1);
+    	end
+    else
+        if ('iconP2.angle' ~= 0) then
+    	    setProperty('iconP2.angle', getProperty('iconP2.angle')+1);
+    	end
+    end
+
+end
+function onUpdatePost()
+ setProperty("iconP1.scale.y", (getProperty("iconP1.scale.y") - 1) / 0.4 + 1)
+ setProperty("iconP2.scale.y", (getProperty("iconP2.scale.y") - 1) / 0.4 + 1)
+ setProperty("iconP1.y", 496 + (getProperty("iconP1.scale.y") * 75))
+ setProperty("iconP2.y", 496 + (getProperty("iconP2.scale.y") * 75))
+ if getPropertyFromClass('ClientPrefs', 'downScroll') == true then
+ setProperty("iconP1.y", -66 + (getProperty("iconP1.scale.y") * 75))
+ setProperty("iconP2.y", -66 + (getProperty("iconP2.scale.y") * 75))
+end
+end
